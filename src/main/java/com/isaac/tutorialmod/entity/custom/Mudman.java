61a2ce93e4ae8167135f48b2e2b927ef084529bc
Azteca09 <isaac306525@gmail.com>
2022-12-26 23:18:17 -0800

    Starting to understand how to make mudman launch entities

--- src/main/java/com/isaac/tutorialmod/entity/custom/Mudman.java
+++ src/main/java/com/isaac/tutorialmod/entity/custom/Mudman.java
@@ -11,12 +11,15 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.IronGolem;
+import net.minecraft.world.entity.item.FallingBlockEntity;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.SmallFireball;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -25,6 +28,8 @@ import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
+
+import net.minecraft.world.entity.item.FallingBlockEntity;
 
 public class Mudman extends Monster implements IAnimatable {
     private AnimationFactory factory = new AnimationFactory(this);
@@ -53,13 +58,13 @@ public class Mudman extends Monster implements IAnimatable {
         this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
 
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, false));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, false));
         this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, IronGolem.class, true));
         this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, Creeper.class, true));
     }
 
     static class MudmanPlacesDirt extends Goal {
-        private final LivingEntity mudman;
+        private final Mudman mudman;
         private final Level level;
         private int timeUntilNextPlacement = 0;
 
@@ -67,12 +72,14 @@ public class Mudman extends Monster implements IAnimatable {
             //return true;
         //}
 
-        public MudmanPlacesDirt(Monster mudman) {
+        public MudmanPlacesDirt(Mudman mudman) {
             this.mudman = mudman;
             this.level = mudman.level;
         }
         public boolean canUse() {
-            return timeUntilNextPlacement-- <= 0;
+
+            LivingEntity livingentity = mudman.getTarget();
+            return livingentity != null && livingentity.isAlive() && this.mudman.canAttack(livingentity);
         }
         @Override
         public void tick() {
@@ -86,8 +93,44 @@ public class Mudman extends Monster implements IAnimatable {
 
             // Place a dirt block at the chosen position
             Level level = this.mudman.level;
+
+
             level.setBlock(targetPos, Blocks.DIRT.defaultBlockState(), 2);
-            timeUntilNextPlacement = 20;
+
+            LivingEntity livingentity = mudman.getTarget();
+
+            /*
+
+            double d0 = this.mudman.distanceToSqr(livingentity);
+            double d1 = livingentity.getX() - this.mudman.getX();
+            double d2 = livingentity.getY(0.5D) - this.mudman.getY(0.5D);
+            double d3 = livingentity.getZ() - this.mudman.getZ();
+            double d4 = Math.sqrt(Math.sqrt(d0)) * 0.5D;
+
+            SmallFireball smallfireball = new SmallFireball(this.mudman.level, this.mudman, this.mudman.getRandom().triangle(d1, 2.297D * d4), d2, this.mudman.getRandom().triangle(d3, 2.297D * d4));
+            smallfireball.setPos(smallfireball.getX(), this.mudman.getY(0.5D) + 0.5D, smallfireball.getZ());
+
+
+            this.mudman.level.addFreshEntity(smallfireball);
+
+             */
+
+            BlockState sandBlock = Blocks.SAND.defaultBlockState();
+
+            //this.mudman.level, this.mudman.getX(), this.mudman.getY(), this.mudman.getZ(), Blocks.SAND.defaultBlockState());
+
+            //FallingBlockEntity fallingBlock = new FallingBlockEntity(this.mudman.level, this.mudman.getX(), this.mudman.getY(), this.mudman.getZ(), Blocks.SAND.defaultBlockState());
+
+            FallingBlockEntity fallingBlock = new FallingBlockEntity(this.mudman.level, 0, 0, 10, sandBlock);
+
+            // set the FallingBlockEntity's motion to move it along an arch-shaped path
+            //fallingBlock.moveTo(this.mudman.getX(), this.mudman.getY()+10, this.mudman.getZ());
+            //fallingBlock.setVelocity(0, 0.1, 0);
+
+            // add the FallingBlockEntity to the world
+            this.mudman.level.addFreshEntity(fallingBlock);
+            t
+                    imeUntilNextPlacement = 20;
         }
     }
 
